aba78e519346beaedf611e5f2e9a970554ab6e55
Steven Young <steven97@me.com>
2023-02-22 11:04:27 -0500

    Missing files

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,19 @@
+const domains = [
+    "https://csconfederation.com",
+    "https://www.csconfederation.com",
+    "http://localhost:3000"
+  ]
+
+window.addEventListener("message", messageHandler, false);function messageHandler(event) {
+    currentColorScheme = JSON.parse(localStorage.getItem("/.__palette"))
+
+    if (domains.includes(event.origin)){
+        if(event.data === "light" && currentColorScheme.index == 1){
+            window.localStorage.setItem("/.__palette", '{"index":0,"color":{"scheme":"default","primary":"","accent":""}}')
+            location.reload()
+        }else if(event.data === "dark" && currentColorScheme.index == 0){
+            window.localStorage.setItem("/.__palette", '{"index":1,"color":{"scheme":"slate","primary":"","accent":""}}')
+            location.reload()
+        }
+    }
+  }
